45ec1594fa663bbaec916eedeac939305847ecdb
Max Shp <mad-metallist@yandex.ru>
2025-02-04 18:51:25 +0300

    Description edited

--- web/template.php
+++ web/template.php
@@ -2,11 +2,11 @@
 <html lang="ru">
 <head>
   <meta charset="UTF-8">
-  <title>О сайте</title>
+  <title>Авторизация</title>
 </head>
 <body>
-  <h1>О сайте</h1>
-  <p>Автор: Максим</p>
+  <h1>Форма авторизации</h1>
+  <p>Автор: Максим Шипицин</p>
   <form action="post.php" method="post">
     <label for="username">Введите ваше имя:</label>
     <input type="text" id="username" name="username" required>
